624d8192c57c1c2404d8ef065cc53e251f5b3c3a
Guedxx <pedrogg@id.uff.br>
2025-10-02 23:38:15 -0300

    add: deal with CLI input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 mod deck;
 mod player;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+
+
 fn main() {
     clearscreen::clear().expect("failed to clear screen");
-    println!("Let's play black_jack")
 
-}
+    println!("Let's play black_jack");
+    
+    let playing = Arc::new(AtomicBool::new(true));
+    let p = playing.clone();
+
+    ctrlc::set_handler(move || {
+        println!("\n exiting black_jack... for now.");
+        p.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+
+    while playing.load(Ordering::SeqCst) {
+        if !playing.load(Ordering::SeqCst) {
+            break;
+        }   
+    }
+}   
