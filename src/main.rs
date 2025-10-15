1d4329543716fdadfca7972423d4a7e19db56775
Guedxx <pedrogg@id.uff.br>
2025-10-15 20:38:29 -0300

    fix: insta blackjack does not print cards

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ fn main() {
 
     let mut lost = 0;
 
-
     println!("Welcome to Black Crab! The best Blackjack game in the world!\n\n");
 
     loop {
@@ -53,86 +52,83 @@ fn main() {
         dealer.add_hand(deck.deal().unwrap());
         dealer.add_hand(deck.deal().unwrap());
 
-        let player_sum = player.sum_value_cards();
+        // player's turn
+        loop {
+            clearscreen::clear().expect("failed to clear screen");
 
-        if player_sum == 21 {
-            println!("Blackjack! You win!");
-            player.money += num + (num as f32 * 1.5) as i32;
-        } else {
-            // player's turn
+            let player_sum = player.sum_value_cards();
+
+            println!("Table's cards:");
+            dealer.print_dealer_cards();
+            player.print_cards();
+            println!("Sums to {}\n", player_sum);
+
+            if player_sum > 21 {
+                println!("Bust! You lose!");
+                lost = 1;
+                break;
+            }
+            if player_sum == 21 {
+                println!("Blackjack! You win!");
+                player.money += num + (num as f32 * 1.5) as i32;
+            }
+
+            println!("Hit or Stand? [h/s]:");
+
+            let mut round_input = String::new();
+            io::stdin()
+                .read_line(&mut round_input)
+                .expect("Failed to read line");
+
+            let player_input = round_input.trim();
+
+            match player_input {
+                "h" => player.add_hand(deck.deal().unwrap()),
+                "s" => break,
+                _ => println!("Invalid input"),
+            }
+        }
+
+        // dealer's turn
+        if lost == 0 {
             loop {
                 clearscreen::clear().expect("failed to clear screen");
 
+                let dealer_sum = dealer.sum_value_cards();
                 let player_sum = player.sum_value_cards();
 
                 println!("Table's cards:");
-                dealer.print_dealer_cards();
+                dealer.print_cards();
                 player.print_cards();
-                println!("Sums to {}\n", player_sum);
+                println!("Dealer sums to {}\n", dealer_sum);
+                println!("Player sums to {}\n", player_sum);
 
-                if player_sum > 21 {
-                    println!("Bust! You lose!");
-                    lost = 1;
-                    break;
-                }
-
-                println!("Hit (h) or Stand (s)?:");
-
-                let mut round_input = String::new();
-                io::stdin()
-                    .read_line(&mut round_input)
-                    .expect("Failed to read line");
-
-                let player_input = round_input.trim();
-
-                match player_input {
-                    "h" => player.add_hand(deck.deal().unwrap()),
-                    "s" => break,
-                    _ => println!("Invalid input"),
-                }
-            }
-
-            // dealer's turn
-            if lost == 0 {
-                loop {
-                    clearscreen::clear().expect("failed to clear screen");
-
-                    let dealer_sum = dealer.sum_value_cards();
-                    let player_sum = player.sum_value_cards();
-
-                    println!("Table's cards:");
-                    dealer.print_cards();
-                    player.print_cards();
-                    println!("Dealer sums to {}\n", dealer_sum);
-                    println!("Player sums to {}\n", player_sum);
-
-                    // The dealer must hit until their score is 17 or higher.
-                    if dealer_sum < 17 {
-                        println!("Dealer hits");
-                        dealer.add_hand(deck.deal().unwrap());
-                        thread::sleep(time::Duration::from_millis(1000));
-                    } else {
-                        // Compare the dealer's and player's scores to determine the winner.
-                        match dealer_sum.cmp(&player_sum) {
-                            std::cmp::Ordering::Greater => {
-                                if dealer_sum > 21 {
-                                    println!("Dealer busts! You win!");
-                                    player.money += num * 2;
-                                } else {
-                                    println!("Dealer wins!");
-                                }
-                            }
-                            std::cmp::Ordering::Less => {
-                                println!("You win!");
+                // The dealer must hit until their score is 17 or higher.
+                if dealer_sum < 17 {
+                    println!("Dealer hits");
+                    dealer.add_hand(deck.deal().unwrap());
+                    thread::sleep(time::Duration::from_millis(1000));
+                } else {
+                    // Compare the dealer's and player's scores to determine the winner.
+                    match dealer_sum.cmp(&player_sum) {
+                        std::cmp::Ordering::Greater => {
+                            if dealer_sum > 21 {
+                                println!("Dealer busts! You win!");
                                 player.money += num * 2;
-                            }
-                            std::cmp::Ordering::Equal => {
-                                println!("Push!");
-                                player.money += num;
+                            } else {
+                                println!("Dealer wins!");
                             }
                         }
-                        break;
+                        std::cmp::Ordering::Less => {
+                            println!("You win!");
+                            player.money += num * 2;
+                        }
+                        std::cmp::Ordering::Equal => {
+                            println!("Push!");
+                            player.money += num;
+                        }
                     }
+                    break;
                 }
             }
         }
